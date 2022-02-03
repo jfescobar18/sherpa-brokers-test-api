be16e0fd8eac9eba8cb0d2ef046019b6dae9c027
Francisco Escobar <jfescobar18@hotmail.com>
2022-02-02 23:45:51 -0600

    emailer added

---
+++ src/Utils/ComposeEmail.js
@@ -0,0 +1,46 @@
+const db = require("../Sequelize/database");
+const { ConsoleError } = require("./ConsoleConfig");
+import { getCategoryName } from "../Controllers/CategoriesController";
+import { initEmail } from "../Utils/Emailer";
+
+exports.checkProductChanges = (productBody) => {
+    try {
+        const _product = await db.Products.findAll({
+            where: {
+                ProductId: productBody.ProductId
+            }
+        });
+
+        const product = _product.length > 0 ? _product[0].dataValues : null;
+
+        let productHasChanged = false;
+        let changesHistory = '';
+
+        if (product.ProductName !== productBody.ProductName) {
+            productHasChanged = true;
+            changesHistory +=
+                `<p>The name of this product has been changed<br>
+            before: <b>${product.ProductName}</b>, now: <b>${productBody.ProductName}</b>`
+        }
+
+        if (product.ProductPrice !== productBody.ProductPrice) {
+            productHasChanged = true;
+            changesHistory +=
+                `<p>The price of this product has been changed<br>
+            before: <b>${product.ProductPrice}</b>, now: <b>${productBody.ProductPrice}</b>`
+        }
+
+        if (product.CategoryId !== productBody.CategoryId) {
+            productHasChanged = true;
+            changesHistory +=
+                `<p>The price of this product has been changed<br>
+            before: <b>${getCategoryName(product.CategoryId)}</b>, now: <b>${getCategoryName(productBody.CategoryId)}</b>`
+        }
+
+        if (productHasChanged) {
+            initEmail(product.ProductName, changesHistory);
+        }
+    } catch (error) {
+        console.log(ConsoleError, error.message);
+    }
+}
---
+++ src/Utils/Emailer.js
@@ -0,0 +1,66 @@
+const nodemailer = require("nodemailer");
+const db = require("../Sequelize/database");
+const path = require('path');
+const fs = require("fs");
+
+exports.initEmail = (ProductName, Body) => {
+    return new Promise(function (resolve, reject) {
+        try {
+            const email = composeEmail(ProductName, Body);
+            resolve(sendEmail(email.subject, email.text, email.body));
+        }
+        catch (error) {
+            var reason = { "error": "nodemailer error", "message": error.message }
+            reject(reason);
+        }
+    });
+}
+
+const composeEmail = (ProductName, Body) => {
+    try {
+        let emailComposed = {};
+
+        emailComposed.subject = "A product has changed";
+        emailComposed.text = `New changes on the product: ${ProductName}`;
+        emailComposed.body = fs.readFileSync(path.join(__dirname, "./Mailing/Notify.html"), "utf8");
+        emailComposed.body = emailComposed.body.toString();
+        emailComposed.body = emailComposed.body.replace("{ProductName}", ProductName);
+        emailComposed.body = emailComposed.body.replace("{Body}", Body);
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+}
+
+const sendEmail = async function (subject, text, html) {
+    try {
+        let config = {
+            host: process.env.HOST_SMTP,
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.USER_SMTP,
+                pass: process.env.PASSWORD_SMTP
+            }
+        };
+
+        const transporter = nodemailer.createTransport(config);
+        const admins = await db.Admins.findAll();
+        let info;
+
+        admins.forEach(admin => {
+            info = await transporter.sendMail({
+                from: `Sherpa Brokers Test 🗃 <${process.env.USER_SMTP}>`,
+                to: admin.dataValues.AdminUsername,
+                subject: subject,
+                text: text,
+                html: html,
+            });
+        });
+
+        return info;
+    }
+    catch (error) {
+        throw { "error": error.message };
+    }
+}
