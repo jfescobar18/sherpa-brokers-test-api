8e16b97f83867a7b38827d8e15a97aea53bc34ed
Francisco Escobar <jfescobar18@hotmail.com>
2022-02-02 22:27:22 -0600

    db updated

--- src/Models/init-models.js
+++ src/Models/init-models.js
@@ -1,17 +1,22 @@
 var DataTypes = require("sequelize").DataTypes;
 var _Admins = require("./Admins");
 var _Categories = require("./Categories");
+var _Products = require("./Products");
 var _ProductsChangeLog = require("./ProductsChangeLog");
 
 function initModels(sequelize) {
     var Admins = _Admins(sequelize, DataTypes);
     var Categories = _Categories(sequelize, DataTypes);
+    var Products = _Products(sequelize, DataTypes);
     var ProductsChangeLog = _ProductsChangeLog(sequelize, DataTypes);
 
+    Products.belongsTo(Categories, { as: "Category", foreignKey: "CategoryId"});
+    Categories.hasMany(Products, { as: "Products", foreignKey: "CategoryId"});
 
     return {
         Admins,
         Categories,
+        Products,
         ProductsChangeLog,
     };
 }
